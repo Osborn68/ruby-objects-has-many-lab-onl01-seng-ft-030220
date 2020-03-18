4fbbf9c5bc83441893d8995a1fe23a505b59677c
Christopher Osborn <z68ozz@gmail.com>
2020-03-18 23:41:51 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,18 +2,7 @@ class Song
   
   attr_accessor :title
   
-  @@all = []
+
   
   def initialize(name)
     @name = name
-    @@all << @name
-  end
-  
-  def self.all
-    @@all
-  end
-  
-  def artist_name
-    song.artist = self
-  end
-end
