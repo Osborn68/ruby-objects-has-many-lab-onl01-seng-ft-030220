a980a86e692ce305e64b2269e0d264c7888d361a
Christopher Osborn <z68ozz@gmail.com>
2020-03-18 23:10:50 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,15 +1,16 @@
 class Artist 
   
   attr_accessor :name 
+  attr_reader :songs
   
   def initialize(name)
     @name = name
     @songs = []
   end
   
-  def songs
-    @songs
-  end
+  #def songs
+  #  @songs
+ # end
 
 def add_song(song)
    @song << song
