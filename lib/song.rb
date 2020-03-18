b3a4141c1d0021bc6a568a7d39ed88d630594043
Christopher Osborn <z68ozz@gmail.com>
2020-03-18 23:59:54 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,7 +2,22 @@ class Song
   
   attr_accessor :name, :artist
   
-def initialize(name)
+  @@all = []
+  
+  
+  def initialize(name)
     @name = name
+    @@all << self 
 end 
+
+  def self.all
+    @@all
+  end
+  
+  def artist_name
+    artist.name if artist
+  end
+
+
+
 end
