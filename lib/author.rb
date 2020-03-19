7f3f26413f82ed5bb9e8621ff05322097d7b6d06
Christopher Osborn <z68ozz@gmail.com>
2020-03-19 00:15:54 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -9,12 +9,23 @@ class Author
     @@all << self
   end
   
-  def self.posts
+  def self.all 
     @@all
   end
     
   def add_post(post)
     post.author = self
   end
-    
+  
+  def add_post_by_title(title) 
+    post = post.new(title)
+    post.author = self
+  end 
+  
+  def posts 
+    post.all.select {|post| post.author == self}
+  end
+  
+  def self.post_count 
+    post.all.count
   end
