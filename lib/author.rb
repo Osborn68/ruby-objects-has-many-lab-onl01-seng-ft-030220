4183de6e892ec7c9c04174719a5618082e5218f2
Christopher Osborn <z68ozz@gmail.com>
2020-03-19 00:22:14 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -19,14 +19,14 @@ class Author
   end
   
   def add_post_by_title(title) 
-    post = post.new(title)
+    post = Post.new(title)
     post.author = self
   end 
   
  
   
   def self.post_count 
-    post.all.count
+    Post.all.count
   end 
   
 end
